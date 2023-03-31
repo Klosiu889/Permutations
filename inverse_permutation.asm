261453fb9a9c959dd97958a12016e28a4da6f0fd
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-03-31 12:35:54 +0200

    ASM file

---
+++ inverse_permutation.asm
@@ -0,0 +1,43 @@
+global inverse_permutation
+
+section .text
+
+inverse_permutation:
+	mov rax, rdi
+	test rax, rax
+	js .bad_result
+	mov rdx, 0
+.loop_negative:
+	cmp word [rsi + rdx * 4 + 0], 0 
+	js .bad_result
+	inc rdx
+	cmp rdi, rdx
+	jne .loop_negative
+	mov rdx, 0
+.loop_permutation:
+	mov ecx, [rsi + rdx * 4 + 0]
+	test ecx, ecx
+	jns .positive
+	not ecx
+	mov eax, [rsi + rcx * 4 + 0]
+	not ecx
+.positive:
+	mov eax, [rsi + rcx * 4 + 0]
+	test eax, eax
+	js .bad_result
+	not rax
+	mov [rsi + rcx * 4 + 0], eax
+	inc rdx
+	cmp rdi, rdx
+	jne .loop_permutation
+	
+	
+	jmp .good_result
+
+.good_result:
+	mov rax, 1
+	ret
+
+.bad_result:
+	mov rax, 0
+	ret
