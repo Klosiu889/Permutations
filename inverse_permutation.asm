38e753ae8c2bbbde3992c853d8cc86f9c295ef8b
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-01 00:08:51 +0200

    Improvements
    Signed-off-by: Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -34,16 +34,16 @@ inverse_permutation:
 	jne .loop_permutation
 	xor rdx, rdx
 .loop_inverse_permutation:
-    mov rax, [rsi + rdx * 4 + 0]
-    not rax
-    cmp rax, rdx
-    je .next_step
-    mov rcx, [rsi + rax * 4 + 0]
+    ;mov rax, [rsi + rdx * 4 + 0]
+    ;not rax
+    ;cmp rax, rdx
+    ;je .next_step
+    ;mov rcx, [rsi + rax * 4 + 0]
 
-.next_step:
-    inc rdx
-    cmp rdi, rdx
-    jne .loop_inverse_permutation
+;.next_step:
+    ;inc rdx
+ ;   cmp rdi, rdx
+;    jne .loop_inverse_permutation
 .loop_exit:
  	jmp .good_result
 
