715c1c59ab2b319f120299201c79727a9d444208
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-01 18:17:12 +0200

    Reversing permutation
    Signed-off-by: Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -50,7 +50,7 @@ inverse_permutation:
     mov ecx, r8d
     cmp r9d, ecx
     jne .permutation_cycle
-    mov r9d, ecx
+    mov [rsi + rcx * 4 + 0], r9d
 .step:
     mov [rsi + rdx * 4 + 0], eax
     inc rdx
